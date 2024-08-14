438acda7d65b9b2b0de2e1cc8ac1bee612b3c60b
Nick Thomas <nichoth@nichoth.com>
2024-08-14 16:06:50 -0700

    fix exports

--- example/index.ts
+++ example/index.ts
@@ -1,4 +1,6 @@
 import '../src/index.css'
+// testing import syntax + exports
+import '@substrate-system/css-util/visually-hidden'
 
 document.body.innerHTML += `
     <button>example button</button>
